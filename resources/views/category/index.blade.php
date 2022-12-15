f88ae57d2d4f3259cf247b02a8e8424e56629788
Angga Irham <anggairham08@gmail.com>
2022-12-15 16:05:13 +0700

    integrasi stisla

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -1,51 +1,79 @@
-@extends('layout')
- 
-@section('content')
-    <div class="row mt-5">
-        <div class="col-lg-12 margin-tb">
-            <div class="float-start">
-                <h2>Laravel 9 CRUD School Application</h2>
+@extends('layouts.app')
+
+@section('title', 'Blank Page')
+
+@push('style')
+    <!-- CSS Libraries -->
+@endpush
+
+
+@section('main')
+    <div class="main-content">
+        <section class="section">
+            <div class="section-header">
+                <h1>Blank Page</h1>
             </div>
-            <div class="float-end">
-                <a class="btn btn-success" href="{{ route('category.create') }}"> Create New student</a>
+
+            <div class="section-body">
+                @if ($message = Session::get('success'))
+                    <div class="alert alert-success">
+                        <p>{{ $message }}</p>
+                    </div>
+                @endif
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Simple</h4>
+                        <div class="float-right">
+                            <a class="btn btn-success" href="{{ route('category.create') }}"> Create New student</a>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-bordered table-striped tabl-emd">
+                                <tr>
+                                    <th width="5%">No</th>
+                                    <th>Name</th>
+                                    <th width="280px">Action</th>
+                                </tr>
+                                @foreach ($category as $data)
+                                    <tr>
+                                        <td>{{ ++$i }}</td>
+                                        <td>{{ $data->category_name }}</td>
+                                        <td>
+                                            <form action="{{ route('category.destroy', $data->id) }}" method="POST">
+
+                                                <a class="btn btn-xs btn-info"
+                                                    href="{{ route('category.show', $data->id) }}">Show</a>
+
+                                                <a class="btn btn-xs btn-primary"
+                                                    href="{{ route('category.edit', $data->id) }}">Edit</a>
+
+                                                @csrf
+                                                @method('DELETE')
+
+                                                <button type="submit" class="btn btn-xs btn-danger"
+                                                    onclick="return confirm('Are you sure?')">Delete</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </table>
+                        </div>
+                        <div class="row text-center">
+                            {!! $category->links() !!}
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
-        </div>
+        </section>
     </div>
-   
-    @if ($message = Session::get('success'))
-        <div class="alert alert-success">
-            <p>{{ $message }}</p>
-        </div>
-    @endif
-   
-    <table class="table table-bordered">
-        <tr>
-            <th>No</th>
-            <th>Name</th> 
-            <th width="280px">Action</th>
-        </tr>
-        @foreach ($category as $data)
-        <tr>
-            <td>{{ ++$i }}</td>
-            <td>{{ $data->category_name }}</td> 
-            <td>
-                <form action="{{ route('category.destroy',$data->id) }}" method="POST">
-   
-                    <a class="btn btn-info" href="{{ route('category.show',$data->id) }}">Show</a>
-    
-                    <a class="btn btn-primary" href="{{ route('category.edit',$data->id) }}">Edit</a>
-   
-                    @csrf
-                    @method('DELETE')
-      
-                    <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure?')">Delete</button>
-                </form>
-            </td>
-        </tr>
-        @endforeach
-    </table>
-    <div class="row text-center">
-        {!! $category->links() !!}
-    </div>
-      
+
 @endsection
+
+@push('scripts')
+    <!-- JS Libraies -->
+
+    <!-- Page Specific JS File -->
+@endpush
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
 
     <!-- General CSS Files -->
     <link rel="stylesheet"
-        href="{{ asset('library/bootstrap/dist/css/bootstrap.min.css') }}">
+        href="{{ asset('modules/bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
         integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
@@ -46,8 +46,8 @@
             @include('components.header')
 
             <!-- Sidebar -->
-            @include('components.sidebar')
-
+            @include('components.sidebar') 
+            
             <!-- Content -->
             @yield('main')
 
@@ -57,12 +57,12 @@
     </div>
 
     <!-- General JS Scripts -->
-    <script src="{{ asset('library/jquery/dist/jquery.min.js') }}"></script>
-    <script src="{{ asset('library/popper.js/dist/umd/popper.js') }}"></script>
-    <script src="{{ asset('library/tooltip.js/dist/umd/tooltip.js') }}"></script>
-    <script src="{{ asset('library/bootstrap/dist/js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('library/jquery.nicescroll/dist/jquery.nicescroll.min.js') }}"></script>
-    <script src="{{ asset('library/moment/min/moment.min.js') }}"></script>
+    <script src="{{ asset('modules/jquery.min.js') }}"></script>
+    <script src="{{ asset('modules/popper.js') }}"></script>
+    <script src="{{ asset('modules/tooltip.js') }}"></script>
+    <script src="{{ asset('modules/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('modules/nicescroll/dist/jquery.nicescroll.min.js') }}"></script>
+    <script src="{{ asset('modules/moment.min.js') }}"></script>
     <script src="{{ asset('js/stisla.js') }}"></script>
 
     @stack('scripts')
